9a80fc126b1b4e446197f87b626a668157f59d89
Aybis <muchtara2m@gmail.com>
2020-10-08 19:45:31 +0700

    create roles and permission

---
+++ database/factories/UserHasRolesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\UserHasRoles;
+use Faker\Generator as Faker;
+
+$factory->define(UserHasRoles::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
